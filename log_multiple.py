89c3bbd5f80bd46a23eab20628524f5396096fd0
Alex Kiura <kiuraalex@gmail.com>
2018-05-27 11:45:28 +0300

    [Chore] Fix lint issues

--- log_multiple.py
+++ log_multiple.py
@@ -14,9 +14,14 @@ end = datetime.date(2018, 3, 12)
 
 
 if __name__ == '__main__':
-    dates = [str(day) for day in (start + timedelta(days=num) for num in range(0, (end-start).days + 1)) if day.isoweekday() <=5]
+    dates = [str(day) for day in (
+        start + timedelta(days=num)
+        for num in range(0, (end-start).days + 1)) if day.isoweekday() <= 5]
 
     for date in dates:
-        request = log_hours(date=date, url=url, project_name=project_name, project_tags=project_tags, auth_token=auth_token)
+        request = log_hours(
+            date=date, url=url, project_name=project_name,
+            project_tags=project_tags, auth_token=auth_token
+        )
         if request.status_code is 200 or 201:
             print('Successfully logged for {}'.format(str(date)))
